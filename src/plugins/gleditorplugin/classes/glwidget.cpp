f043adc64d91c273cbd42ed0e73ad55ba2f701fc
andy-fillebrown <andy.fillebrown@gmail.com>
2011-03-21 16:52:39 -0400

    cleanup and add comments in glwidget.cpp

--- src/plugins/gleditorplugin/classes/glwidget.cpp
+++ src/plugins/gleditorplugin/classes/glwidget.cpp
@@ -79,16 +79,18 @@ void GLWidget::setCurrentSplit(GLWidgetSplit *split)
     Q_ASSERT(isValid());
     Q_ASSERT(context() == QGLContext::currentContext());
 
+    // Set old viewport background color to inactive color (light grey).
     GLViewport *vp = d->currentSplit ? d->currentSplit->viewport() : 0;
     if (vp)
         vp->setBackgroundColor(QColor(251, 251, 251));
 
+    // Set current viewport background color to active color (white).
     vp = split->viewport();
     if (vp)
         vp->setBackgroundColor(Qt::white);
 
+    // Update current split and redraw.
     d->currentSplit = split;
-
     updateGL();
 }
 
@@ -178,10 +180,11 @@ void GLWidget::animateGL()
     if (!isAnimating())
         return;
 
+    // Hackish attempt at maintaining 60 fps.
+    // 'd->prevFrameTime' is set in paintGL().
     const qint64 elapsed = d->elapsedTime.elapsed() - d->prevFrameTime;
-    const qint64 nextFrameTime = 20 <= elapsed ? 0 : 20 - elapsed;
+    const qint64 nextFrameTime = 14 <= elapsed ? 0 : 14 - elapsed;
     QTimer::singleShot(nextFrameTime, this, SLOT(animateGL()));
-    d->prevFrameTime = d->elapsedTime.elapsed();
 }
 
 void GLWidget::paintGL()
@@ -205,6 +208,8 @@ void GLWidget::paintGL()
     d->shaderProgram->release();
     Q_CHECK_GLERROR;
     qglPopState();
+
+    d->prevFrameTime = d->elapsedTime.elapsed();
 }
 
 void GLWidget::resizeGL(int width, int height)
@@ -217,6 +222,8 @@ void GLWidget::mousePressEvent(QMouseEvent *event)
 {
     const QPoint pos = event->pos();
 
+    // If left mouse button is pressed on a viewport, make it current.
+    // If left mouse button is pressed on a viewport border, start dragging it.
     if (event->button() == Qt::LeftButton) {
         GLWidgetSplit *split = d->mainSplit;
         while (split->isSplit()) {
@@ -225,15 +232,21 @@ void GLWidget::mousePressEvent(QMouseEvent *event)
             else if (split->splitTwo()->rect().contains(pos))
                 split = split->splitTwo();
             else {
+                // On border.
                 d->draggingSplit = split;
                 split = 0;
                 break;
             }
         }
+        // 'split' is not zero if not on border.
         if (split)
             setCurrentSplit(split);
+
+        return;
     }
-    else if (event->button() == Qt::RightButton) {
+
+    // Start panning or rotating if mouse is not over a viewport border.
+    if (event->button() == Qt::RightButton) {
         d->dragStartScreenPosition = pos;
 
         if (QApplication::keyboardModifiers() & Qt::ControlModifier)
@@ -243,6 +256,10 @@ void GLWidget::mousePressEvent(QMouseEvent *event)
 
         if (d->isPanning || d->isRotating) {
             d->draggingViewport = d->viewportAtPosition(pos);
+
+            // If mouse is over a viewport, d->draggingViewport will be valid,
+            // otherwise the mouse is over a viewport border and panning or
+            // rotating should not be started.
             if (d->draggingViewport)
                 d->prevDragPos = pos;
             else
@@ -256,20 +273,23 @@ void GLWidget::mouseMoveEvent(QMouseEvent *event)
     const QPoint pos = event->pos();
 
     if (d->draggingSplit) {
+        GLWidgetSplit *split = d->draggingSplit;
+
        // Convert mouse position from widget coords to split coords.
        QPoint splitPos = pos;
-       splitPos.rx() -= d->draggingSplit->pos().x();
-       splitPos.ry() -= d->draggingSplit->pos().y();
+       splitPos.rx() -= split->pos().x();
+       splitPos.ry() -= split->pos().y();
 
-       if (d->draggingSplit->isSplitHorizontal())
-           d->draggingSplit->setSplitLocation(qreal(splitPos.y()) / qreal(d->draggingSplit->height()));
+       // Move split location.
+       if (split->isSplitHorizontal())
+           split->setSplitLocation(qreal(splitPos.y()) / qreal(split->height()));
        else
-           d->draggingSplit->setSplitLocation(qreal(splitPos.x()) / qreal(d->draggingSplit->width()));
+           split->setSplitLocation(qreal(splitPos.x()) / qreal(split->width()));
 
-       d->draggingSplit->resize(d->draggingSplit->width(), d->draggingSplit->height());
+       // Update child splits and redraw.
+       split->resize(split->width(), split->height());
        updateGL();
 
-       Q_CHECK_GLERROR;
        return;
     }
 
@@ -288,32 +308,37 @@ void GLWidget::mouseMoveEvent(QMouseEvent *event)
                 break;
             }
         }
-        const Qt::CursorShape currentCursorShape = cursor().shape();
-        Qt::CursorShape desiredCursorShape = Qt::CrossCursor;
+
+        // Figure out which cursor shape to use.
+        Qt::CursorShape cursorShape = Qt::CrossCursor;
         if (isOnBorder)
-            desiredCursorShape = split->isSplitHorizontal() ? Qt::SizeVerCursor
-                                                            : Qt::SizeHorCursor;
-        if (currentCursorShape != desiredCursorShape)
+            cursorShape = split->isSplitHorizontal() ? Qt::SizeVerCursor : Qt::SizeHorCursor;
+
+        // Change cursor shape, if necessary.
+        if (cursor().shape() != desiredCursorShape)
             setCursor(QCursor(desiredCursorShape));
 
-        // Don't fall through to pan/rotate if the cursor is on border.
+        // Don't fall through to pan/rotate if cursor is on border.
         if (isOnBorder)
             return;
     }
 
     if (d->isPanning) {
+        // Calculate drag vector from previous mouse position.
         GLViewport *vp = d->draggingViewport;
         const Point startPt = vp->findPointOnUcs(d->prevDragPos - vp->pos());
         const Point endPt = vp->findPointOnUcs(pos - vp->pos());
         Point dragVec = endPt - startPt;
-        dragVec[1] = -dragVec[1]; // ... don't know why y coord needs negation
+        dragVec[1] = -dragVec[1];
 
         const Point camPos = vp->cameraPosition() + dragVec;
         const Point camTar = vp->cameraTarget() + dragVec;
         vp->setCamera(camPos, camTar, Vector(0.0f, 1.0f, 0.0f));
 
+        // Update previous mouse position and redraw.
         d->prevDragPos = pos;
         updateGL();
+
         return;
     }
 
@@ -322,49 +347,61 @@ void GLWidget::mouseMoveEvent(QMouseEvent *event)
         const QSize size = vp->size();
         const QPoint deltaPos = pos - d->prevDragPos;
 
+        // Pause automatic view updating, since we may be making two calls to
+        // GLViewport::setCamera.
         vp->pushViewAutomaticUpdate();
 
+        // Rotate the camera around the target using the y axis as the axis of
+        // rotation.
         const real dragX = real(deltaPos.x()) / real(size.width());
         if (dragX != 0.0f) {
+            // Calculate rotation matrix using previous mouse position.
             AxisAngle axisAngle(-2.0f * M_PI * dragX, GL::yAxis);
             Matrix m;
             gmtl::setRot(m, axisAngle);
 
+            // Rotate camera's position.
             Point normCamPos = vp->cameraPosition() - vp->cameraTarget();
             gmtl::xform(normCamPos, m, normCamPos);
             Point newCamPos = vp->cameraTarget() + normCamPos;
 
+            // Rotate camera's up vector.
             Vector newCamUpVec;
             gmtl::xform(newCamUpVec, m, vp->cameraUpVector());
 
+            // Apply new position and up vector.
             vp->setCamera(newCamPos, vp->cameraTarget(), newCamUpVec);
         }
 
+        // Rotate the camera around the target using the camera's side vector
+        // as the axis of rotation.
         const real dragY = real(deltaPos.y()) / real(size.height());
         if (dragY != 0.0f) {
-//            Vector fwd = vp->cameraTarget() - vp->cameraPosition();
-//            gmtl::normalize(fwd);
-//            Vector side;
-//            gmtl::cross(side, fwd, vp->cameraUpVector());
-//            gmtl::normalize(side);
+            // Calculate rotation matrix using previous mouse position.
             AxisAngle axisAngle(-2.0f * M_PI * dragY, vp->cameraSideVector());
             Matrix m;
             gmtl::setRot(m, axisAngle);
 
+            // Rotate camera's position.
             Point normCamPos = vp->cameraPosition() - vp->cameraTarget();
             gmtl::xform(normCamPos, m, normCamPos);
             Point newCamPos = vp->cameraTarget() + normCamPos;
 
+            // Rotate camera's up vector.
             Vector newCamUpVec;
             gmtl::xform(newCamUpVec, m, vp->cameraUpVector());
 
+            // Apply new position and up vector.
             vp->setCamera(newCamPos, vp->cameraTarget(), newCamUpVec);
         }
 
+        // Restart automatic view updating.
         vp->popViewAutomaticUpdate();
 
+        // Update previous mouse position and redraw.
         d->prevDragPos = pos;
         updateGL();
+
         return;
     }
 }
@@ -382,6 +419,7 @@ void GLWidget::mouseReleaseEvent(QMouseEvent *event)
 
 void GLWidget::wheelEvent(QWheelEvent *event)
 {
+    // Do nothing if mouse pointer is not over viewport.
     GLViewport *vp = d->viewportAtPosition(event->pos());
     if (!vp) {
         event->ignore();
