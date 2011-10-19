2f99a02fd88faa79f55f24459d04729a742a94e3
Andy Fillebrown (Ubuntu 10.04 LTS) <andy.fillebrown@gmail.com>
2011-10-19 00:59:35 -0400

    add IFactory interface

---
+++ src/plugins/ac_coreplugin/interfaces/ac_ifactory.h
@@ -0,0 +1,43 @@
+/**************************************************************************
+**
+** This file is part of AudioCarver
+**
+** Copyright (c) 2011 Andrew Fillebrown.
+**
+** Contact: Andy Fillebrown (andy.fillebrown@gmail.com)
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file.  Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#ifndef AC_IFACTORY_H
+#define AC_IFACTORY_H
+
+#include <ac_namespace.h>
+#include <ac_object.h>
+
+#include <mi_iunknown.h>
+
+class QVariant;
+
+class IFactory : public QObject
+{
+    Q_OBJECT
+
+public:
+    IFactory();
+
+    static IFactory *instance();
+
+    virtual Object *create(Ac::ItemType type) const = 0;
+
+private:
+    Q_DISABLE_COPY(IFactory)
+};
+
+#endif // AC_IFACTORY_H
--- src/plugins/ac_coreplugin/interfaces/interfaces.pri
+++ src/plugins/ac_coreplugin/interfaces/interfaces.pri
@@ -2,3 +2,11 @@
 HEADERS *= \
     ac_ientity.h \
     ac_imodelitem.h \
+
+SOURCE_PAIRS = \
+    ac_ifactory \
+
+for(pair, SOURCE_PAIRS) {
+    HEADERS *= interfaces/$${pair}.h
+    SOURCES *= interfaces/$${pair}.cpp
+}
