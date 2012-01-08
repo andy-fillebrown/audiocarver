135c7b2cad407303529c2f51c9fdc04871c4cf10
Andy Fillebrown (Ubuntu 10.04 LTS) <andy.fillebrown@gmail.com>
2012-01-08 05:14:43 -0500

    test csound synthesizer file names

--- src/plugins/ac_synthesizer/csound/ac_csoundsynthesizer.cpp
+++ src/plugins/ac_synthesizer/csound/ac_csoundsynthesizer.cpp
@@ -19,8 +19,12 @@
 
 #include <ac_namespace.h>
 
+#include <mi_idatabase.h>
 #include <mi_imodel.h>
 #include <mi_imodelitem.h>
+
+#include <QDir>
+#include <QFileInfo>
 
 #include <csound.h>
 
@@ -32,7 +36,30 @@ public:
         if (!track)
             return;
 
+        const QString database_file_name = IDatabase::instance()->fileName();
+
+        const QString track_name = track->data(Mi::NameRole).toString();
         const QString instrument = track->data(Ac::InstrumentRole).toString();
+
+        IModel *model = IModel::instance();
+        const IModelItem *project_settings = model->itemFromIndex(model->itemIndex(Ac::ProjectSettingsItem));
+        QString output_dir = project_settings->data(Ac::OutputDirectoryRole).toString();
+        QString instrument_dir = project_settings->data(Ac::InstrumentDirectoryRole).toString();
+
+        if (QDir::convertSeparators(output_dir).startsWith(QDir::convertSeparators("./")))
+            output_dir = IDatabase::instance()->fileName() + "/" + output_dir;
+        if (QDir::convertSeparators(instrument_dir).startsWith(QDir::convertSeparators("./")))
+            instrument_dir = IDatabase::instance()->fileName() + "/" + instrument_dir;
+
+        const QString output_file_name = output_dir + "/" + track_name;
+        const QString instrument_file_name = instrument_dir + "/" + instrument;
+
+        QFileInfo output_file_info = QFileInfo(output_file_name);
+        QFileInfo instrument_file_info = QFileInfo(instrument_file_name);
+
+        qDebug() << Q_FUNC_INFO << "filename:" << QDir::convertSeparators(database_file_name);
+        qDebug() << Q_FUNC_INFO << "output:" << QDir::convertSeparators(output_file_info.absoluteFilePath()).toLower();
+        qDebug() << Q_FUNC_INFO << "instrument:" << QDir::convertSeparators(instrument_file_info.absoluteFilePath()).toLower();
 
         const IModelItem *notes = track->findModelItemList(Ac::NoteItem);
         const int n = notes->modelItemCount();
