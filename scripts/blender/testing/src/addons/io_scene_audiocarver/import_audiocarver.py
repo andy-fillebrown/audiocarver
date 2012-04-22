14bd751e3c49e29785fff14237cc7766fa82166d
Andy Fillebrown (Windows 7) <andy.fillebrown@gmail.com>
2012-04-22 06:19:07 -0400

    get timeline object creation working in blender import script

--- scripts/blender/testing/src/addons/io_scene_audiocarver/import_audiocarver.py
+++ scripts/blender/testing/src/addons/io_scene_audiocarver/import_audiocarver.py
@@ -5,8 +5,13 @@ import xml.dom as Dom
 import xml.dom.minidom as Xml
 
 
+layer_1 = (True, False, False, False, False, False, False, False, False, False,
+           False, False, False, False, False, False, False, False, False, False)
+layer_2 = (False, True, False, False, False, False, False, False, False, False,
+           False, False, False, False, False, False, False, False, False, False)
+
 note_count = 1
-note_layer = 18
+note_layer = 8
 note_object_names = {
                      'Note.Root.Empty',
                      'Note.Velocity.Scale.Y',
@@ -14,6 +19,9 @@ note_object_names = {
                      'Note.Head.Mesh',
                      'Note.Tail.Mesh'
                      }
+timeline_count = 1
+timeline_layer = 11
+timeline_parent_layer = 10
 
 
 def clear_ss():
@@ -25,6 +33,8 @@ def import_note(note_node, color):
         print("Xml node is not a note.")
         return
 
+    global layer_1
+    global layer_2
     global note_count
     global note_layer
     global note_object_names
@@ -34,9 +44,6 @@ def import_note(note_node, color):
     # Clear existing selection.
     clear_ss()
 
-    # Turn on the note layer so the note template objects can be selected.
-    bpy.data.scenes[0].layers[note_layer] = True
-
     # Select and duplicate the note template objects.
     bpy.ops.object.select_by_layer(layers = note_layer + 1)
     bpy.ops.object.duplicate()
@@ -44,15 +51,14 @@ def import_note(note_node, color):
     # Rename the new note objects and set their layers.
     for note_object_name in note_object_names:
         obj = bpy.data.objects[note_object_name + '.001']
-        obj.name = obj.name[:-3] + str(note_count)
-        obj.layers[note_layer] = False
+        new_name = obj.name[:-3] + str(note_count)
+        obj.name = new_name
+        clear_ss()
+        bpy.ops.object.select_pattern(pattern = new_name)
         if ("Note.Head.Mesh" == note_object_name):
-            obj.layers[1] = True
+            bpy.ops.object.move_to_layer(layers = layer_2)
         else:
-            obj.layers[0] = True
-
-    # Turn the note template objects layer back off
-    bpy.data.scenes[0].layers[note_layer] = False
+            bpy.ops.object.move_to_layer(layers = layer_1)
 
     # Get the first and last pitch point positions.
     pitch_curve_node = Dom.Node
@@ -130,11 +136,62 @@ def import_track(track_node):
                     import_note(note_node, color)
 
 
+def import_timeline(timeline_node):
+    global timeline_count
+
+    timeline_label = ""
+    timeline_location = 0.0
+    attributes = timeline_node.attributes
+    i = 0
+    while i < attributes.length:
+        attribute = attributes.item(i)
+        if "label" == attribute.name:
+            timeline_label = attribute.value
+        elif "location" == attribute.name:
+            timeline_location = float(attribute.value)
+        i += 1
+
+    if -1 != timeline_label.find("."):
+        return
+    if 0.0 == timeline_location:
+        return
+
+    timeline_count += 1
+
+    clear_ss()
+    bpy.ops.object.select_pattern(pattern = "TimeLine.*.000")
+    bpy.ops.object.duplicate(linked = True)
+
+    clear_ss()
+    timeline_parent = bpy.data.objects["TimeLine.Parent.001"]
+    timeline_parent.name = timeline_parent.name[0:-3] + str(timeline_count)
+    timeline_parent.location[0] = timeline_location
+
+
+def import_timelines(timelines_node):
+    print("\nImporting timelines ...")
+
+    global timeline_layer
+    global timeline_parent_layer
+
+    bpy.data.scenes[0].layers[timeline_layer] = True
+    bpy.data.scenes[0].layers[timeline_parent_layer] = True
+
+    for child_node in timelines_node.childNodes:
+        if "TimeGridLine" == child_node.nodeName:
+            import_timeline(child_node)
+
+    bpy.data.scenes[0].layers[timeline_parent_layer] = False
+
+
 def import_node(xml_node):
     if Dom.Node.TEXT_NODE == xml_node.nodeType:
         return
     if "Track" == xml_node.nodeName:
         import_track(xml_node)
+    elif "TimeGridLineList" == xml_node.nodeName:
+        import_timelines(xml_node)
+        return
     else:
         for child_node in xml_node.childNodes:
             import_node(child_node)
@@ -174,9 +231,25 @@ def load(operator,
     print("\nImporting AudioCarver file ...")
     print(fileName)
     print("... done\n")
+
+    global note_count
+    global note_layer
+
+    # Turn on the note layers so the note template objects can be selected.
+    bpy.data.scenes[0].layers[0] = True
+    bpy.data.scenes[0].layers[1] = True
+    bpy.data.scenes[0].layers[note_layer] = True
+
+    # Parse the input file.
     dom = Xml.parse(fileName)
     for xml_node in dom.childNodes:
         import_node(xml_node)
-    global note_count
+
+    # Turn the note layers back off
+    bpy.data.scenes[0].layers[note_layer] = False
+    bpy.data.scenes[0].layers[1] = False
+    bpy.data.scenes[0].layers[0] = False
+
     note_count = 0
+
     return {'FINISHED'}
