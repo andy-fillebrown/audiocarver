0e12634e59fcae34c6be25b1345c5235a26f3685
Andy Fillebrown (Windows 7) <andy.fillebrown@gmail.com>
2012-04-23 05:58:59 -0400

    speed up blender import script, and cleanup

--- scripts/blender/testing/src/addons/io_scene_audiocarver/import_audiocarver.py
+++ scripts/blender/testing/src/addons/io_scene_audiocarver/import_audiocarver.py
@@ -4,11 +4,14 @@ import bpy
 import xml.dom as Dom
 import xml.dom.minidom as Xml
 
+import time
 
-layer_1 = (True, False, False, False, False, False, False, False, False, False,
-           False, False, False, False, False, False, False, False, False, False)
-layer_2 = (False, True, False, False, False, False, False, False, False, False,
-           False, False, False, False, False, False, False, False, False, False)
+layer_1_mask = (True, False, False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False, False, False)
+layer_2_mask = (False, True, False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False, False, False)
+note_template_layer_mask = (False, False, False, False, False, False, False, False, True, False,
+                            False, False, False, False, False, False, False, False, False, False)
 
 note_count = 1
 note_layer = 8
@@ -23,42 +26,54 @@ timeline_count = 1
 timeline_layer = 11
 timeline_parent_layer = 10
 
+note_template_objects = None
+timeline_template_objects = None
+
 
 def clear_ss():
     bpy.ops.object.select_all(action = 'DESELECT')
 
 
-def import_note(note_node, color):
-    if "Note" != note_node.nodeName:
-        print("Xml node is not a note.")
-        return
+def current_ss():
+    return bpy.context.selected_objects
 
-    global layer_1
-    global layer_2
+
+def select_note_template_objects():
+    global note_template_objects
+
+    clear_ss()
+    for obj in note_template_objects:
+        obj.select = True
+
+
+def select_timeline_template_objects():
+    global timeline_template_objects
+
+    clear_ss()
+    for obj in timeline_template_objects:
+        obj.select = True
+
+
+def import_note(note_node, color):
     global note_count
     global note_layer
     global note_object_names
 
+    if "Note" != note_node.nodeName:
+        print("Xml node is not a note.")
+        return
+
     note_count += 1
 
-    # Clear existing selection.
-    clear_ss()
-
     # Select and duplicate the note template objects.
-    bpy.ops.object.select_by_layer(layers = note_layer + 1)
+    select_note_template_objects()
     bpy.ops.object.duplicate()
 
-    # Rename the new note objects and set their layers.
+    # Rename the new note objects.
     for note_object_name in note_object_names:
-        obj = bpy.data.objects[note_object_name + '.001']
-        new_name = obj.name[:-3] + str(note_count)
+        obj = bpy.data.objects["Template." + note_object_name + '.001']
+        new_name = obj.name[9:-3] + str(note_count)
         obj.name = new_name
-        clear_ss()
-        bpy.ops.object.select_pattern(pattern = new_name)
-        if ("Note.Head.Mesh" == note_object_name):
-            bpy.ops.object.move_to_layer(layers = layer_2)
-        else:
-            bpy.ops.object.move_to_layer(layers = layer_1)
 
     # Get the first and last pitch point positions.
     pitch_curve_node = Dom.Node
@@ -120,15 +135,22 @@ def import_note(note_node, color):
 
 
 def import_track(track_node):
-    print("\nImporting track ...")
-    color = '#ffffff'
+    # Read the track's attributes.
+    color = "#ffffff"
+    track_name = ""
     i = 0
     attributes = track_node.attributes
     while i < attributes.length:
         attribute = attributes.item(i)
         if "color" == attribute.name:
             color = attribute.value
+        if "name" == attribute.name:
+            track_name = attribute.value
         i += 1
+
+    print(" \"" + track_name + "\"")
+
+    # Read the track's note list.
     for child_node in track_node.childNodes:
         if "NoteList" == child_node.nodeName:
             for note_node in child_node.childNodes:
@@ -151,6 +173,8 @@ def import_timeline(timeline_node):
             timeline_location = float(attribute.value)
         i += 1
 
+    if "" == timeline_label:
+        return
     if -1 != timeline_label.find("."):
         return
     if 0.0 == timeline_location:
@@ -158,8 +182,7 @@ def import_timeline(timeline_node):
 
     timeline_count += 1
 
-    clear_ss()
-    bpy.ops.object.select_pattern(pattern = "TimeLine.*.000")
+    select_timeline_template_objects()
     bpy.ops.object.duplicate(linked = True)
 
     clear_ss()
@@ -169,10 +192,10 @@ def import_timeline(timeline_node):
 
 
 def import_timelines(timelines_node):
-    print("\nImporting timelines ...")
-
     global timeline_layer
     global timeline_parent_layer
+
+    print("\nImporting time lines ...")
 
     bpy.data.scenes[0].layers[timeline_layer] = True
     bpy.data.scenes[0].layers[timeline_parent_layer] = True
@@ -197,59 +220,70 @@ def import_node(xml_node):
             import_node(child_node)
 
 
-def trace_node(xml_node, tabCount = 0):
-    if Dom.Node.TEXT_NODE == xml_node.nodeType:
-        return
-    tabs = ""
-    i = 0
-    while i < tabCount:
-        i += 1
-        tabs += "    "
-    attribute_string = " "
-    i = 0
-    attributes = xml_node.attributes
-    while i < attributes.length:
-        attribute = attributes.item(i)
-        if 0 < i:
-            attribute_string += ", "
-        i += 1
-        attribute_string += attribute.name
-        attribute_string += " = "
-        attribute_string += attribute.value
-    if attribute_string == " ":
-        attribute_string = ""
-    print(tabs + "<" + xml_node.nodeName + attribute_string + ">")
-    tabCount += 1
-    for xml_node in xml_node.childNodes:
-        trace_node(xml_node, tabCount)
-    tabCount -= 1
-
-
 def load(operator,
          context,
-         fileName):
-    print("\nImporting AudioCarver file ...")
-    print(fileName)
-    print("... done\n")
-
+         file_name):
+    global layer_1_mask
+    global layer_2_mask
     global note_count
     global note_layer
+    global note_template_layer_mask
+    global note_template_objects
+    global timeline_count
+    global timeline_template_objects
+
+    print("\nImporting AudioCarver file", file_name, "...")
+
+    start_time = time.time()
+
+    # Store the current selection set so it can be restored later.
+    cur_ss = current_ss()
 
     # Turn on the note layers so the note template objects can be selected.
     bpy.data.scenes[0].layers[0] = True
     bpy.data.scenes[0].layers[1] = True
     bpy.data.scenes[0].layers[note_layer] = True
 
+    # Set note template objects list.
+    clear_ss()
+    bpy.ops.object.select_pattern(pattern = "Template.Note.*.000")
+    note_template_objects = current_ss()
+
+    # Set timeline template objects list.
+    clear_ss()
+    bpy.ops.object.select_pattern(pattern = "TimeLine.*.000")
+    timeline_template_objects = current_ss()
+
+    print("\nImporting tracks ...")
+
     # Parse the input file.
-    dom = Xml.parse(fileName)
+    dom = Xml.parse(file_name)
     for xml_node in dom.childNodes:
         import_node(xml_node)
+
+    # Move the note objects off the note template object layer.
+    clear_ss()
+    bpy.ops.object.select_by_layer(layers = note_layer + 1)
+    bpy.ops.object.move_to_layer(layers = layer_1_mask)
+    clear_ss()
+    bpy.ops.object.select_pattern(pattern = "Note.Head.Mesh.*")
+    bpy.ops.object.move_to_layer(layers = layer_2_mask)
+    select_note_template_objects()
+    bpy.ops.object.move_to_layer(layers = note_template_layer_mask)
 
     # Turn the note layers back off
     bpy.data.scenes[0].layers[note_layer] = False
     bpy.data.scenes[0].layers[1] = False
     bpy.data.scenes[0].layers[0] = False
 
-    note_count = 0
+    # Restore the original selection set.
+    clear_ss()
+    for obj in cur_ss:
+        obj.select = True
+
+    print("\n... done in %.3f seconds" % (time.time() - start_time))
+
+    note_count = 1
+    timeline_count = 1
 
     return {'FINISHED'}
