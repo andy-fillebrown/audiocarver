935266d4dfc0c606ec9cf150dc275299ce7ca1d4
Andy Fillebrown (Linux Mint 14) <andy.fillebrown@gmail.com>
2013-07-02 15:16:15 -0400

    move math functions from csound synthesizer plugin to mi core plugin

--- src/plugins/ac_csound_synthesizer/session/ac_csound_synthesizer.cpp
+++ src/plugins/ac_csound_synthesizer/session/ac_csound_synthesizer.cpp
@@ -18,6 +18,7 @@
 #include "ac_csound_synthesizer.h"
 #include <ac_core_namespace.h>
 #include <ac_core_point.h>
+#include <mi_core_math.h>
 #include <mi_core_utilities.h>
 #include <idatabase.h>
 #include <imodelitem.h>
@@ -29,81 +30,6 @@
 
 using namespace Ac;
 using namespace Mi;
-
-static bool isPowerOfTwo(int n)
-{
-    const int size = 8 * sizeof(int);
-    for (int i = 1;  i < size;  i++)
-        if (n == (1 << i))
-            return true;
-    return false;
-}
-
-static int toPowerOfTwo(int n, int min = 4)
-{
-    if (n < min)
-        n = min;
-    if (isPowerOfTwo(n))
-        return n;
-    const int size = 8 * sizeof(int);
-    int i = 0;
-    for (;  i < size;  i++) {
-        if (n == 0) {
-            n = 1 << i;
-            return n;
-        }
-        n = n >> 1;
-        if (isPowerOfTwo(n)) {
-            n = n << (i + 2);
-            return n;
-        }
-    }
-    return min;
-}
-
-static qreal intersectionY(const qreal &x1, // start x
-                           const qreal &x2, // end x
-                           const qreal &y1, // start y
-                           const qreal &y2, // end y
-                           const qreal &intersectionX)
-{
-    qreal intY = 0.0;
-    if (y1 == y2)
-        intY = y1;
-    else {
-        const qreal x2_x1 = x2 - x1;
-        const qreal intX_x1 = intersectionX - x1;
-        if ((x2_x1 == 0.0) || (intX_x1 == 0.0))
-            intY = y1;
-        else
-            intY = y1 + (((y2 - y1) / x2_x1) * intX_x1);
-    }
-    return intY;
-}
-
-static QPointF bezierPoint(const QPointF &pt1, // start point
-                           const QPointF &pt2, // control point
-                           const QPointF &pt3, // end point
-                           const qreal t)
-{
-    const qreal mu2 = t * t;
-    const qreal mum1 = 1 - t;
-    const qreal mum12 = mum1 * mum1;
-    return QPointF(pt1.x() * mum12 + 2 * pt2.x() * mum1 * t + pt3.x() * mu2,
-                   pt1.y() * mum12 + 2 * pt2.y() * mum1 * t + pt3.y() * mu2);
-}
-
-inline qreal round(const qreal from, const qreal to)
-{
-    const qreal r = fmod(from, to);
-    const qreal td2 = to / qreal(2);
-    if (r == 0)
-        return from;
-    else if (r < td2)
-        return from - r;
-    else
-        return from + (to - r);
-}
 
 static void writeTableFile(const QString &fileName, const PointList &points, int valueCount)
 {
@@ -135,7 +61,7 @@ static void writeTableFile(const QString &fileName, const PointList &points, int
                 || j == n_points - 1) {
             // Calculate line using 2 points.
             const QPointF to_point = normalized_points[j] - point_offset;
-            const qreal to_x = round(to_point.x(), div_x);
+            const qreal to_x = roundToNearest(to_point.x(), div_x);
             const qreal to_y = to_point.y();
             const int n_values = (to_x / div_x) + 0.5;
             for (int k = 0;  k < n_values;  ++k) {
--- src/plugins/mi_core/tools/mi_core_math.h
+++ src/plugins/mi_core/tools/mi_core_math.h
@@ -18,7 +18,7 @@
 #ifndef MI_CORE_MATH_H
 #define MI_CORE_MATH_H
 
-#include <qglobal.h>
+#include <QPointF>
 #include <math.h>
 
 namespace Mi {
@@ -33,6 +33,69 @@ inline qreal roundToNearest(const qreal from, const qreal to)
         return from - r;
     else
         return from + (to - r);
+}
+
+inline bool isPowerOfTwo(int n)
+{
+    const int size = 8 * sizeof(int);
+    for (int i = 1;  i < size;  i++)
+        if (n == (1 << i))
+            return true;
+    return false;
+}
+
+inline int toPowerOfTwo(int n, int min = 4)
+{
+    if (n < min)
+        n = min;
+    if (isPowerOfTwo(n))
+        return n;
+    const int size = 8 * sizeof(int);
+    int i = 0;
+    for (;  i < size;  i++) {
+        if (n == 0) {
+            n = 1 << i;
+            return n;
+        }
+        n = n >> 1;
+        if (isPowerOfTwo(n)) {
+            n = n << (i + 2);
+            return n;
+        }
+    }
+    return min;
+}
+
+inline qreal intersectionY(const qreal &x1, // start x
+                           const qreal &x2, // end x
+                           const qreal &y1, // start y
+                           const qreal &y2, // end y
+                           const qreal &intersectionX)
+{
+    qreal intY = 0.0;
+    if (y1 == y2)
+        intY = y1;
+    else {
+        const qreal x2_x1 = x2 - x1;
+        const qreal intX_x1 = intersectionX - x1;
+        if ((x2_x1 == 0.0) || (intX_x1 == 0.0))
+            intY = y1;
+        else
+            intY = y1 + (((y2 - y1) / x2_x1) * intX_x1);
+    }
+    return intY;
+}
+
+inline QPointF bezierPoint(const QPointF &pt1, // start point
+                           const QPointF &pt2, // control point
+                           const QPointF &pt3, // end point
+                           const qreal t)
+{
+    const qreal mu2 = t * t;
+    const qreal mum1 = 1 - t;
+    const qreal mum12 = mum1 * mum1;
+    return QPointF(pt1.x() * mum12 + 2 * pt2.x() * mum1 * t + pt3.x() * mu2,
+                   pt1.y() * mum12 + 2 * pt2.y() * mum1 * t + pt3.y() * mu2);
 }
 
 }
